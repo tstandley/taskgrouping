e949f55730e0d7b2b985897551fdc28795ef0f59
Abhinav Rangarajan <abhinavrangarajan@Abhinavs-MacBook-Pro.local>
2020-05-23 17:38:38 -0400

    init

--- model_definitions/__init__.py
+++ model_definitions/__init__.py
@@ -2,4 +2,4 @@ from .xception_taskonomy_new import *
 from .xception_taskonomy_joined_decoder import *
 from .xception_taskonomy_small import *
 from .resnet_taskonomy import *
-
+from .bug_net import *
