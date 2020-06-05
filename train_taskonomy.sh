ab63ef9fe70755146a6bd457dd33a8679abc81df
naagnes <naagnes@stanford.edu>
2020-06-05 13:54:55 +0000

    added train and val txt files

--- train_taskonomy.sh
+++ train_taskonomy.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
 # Change settings here
-data_dir="../taskonomy-small/"
+# data_dir="../taskonomy-small/"
 #data_dir="/home/ar562/taskonomy-sample-model-1/"
+data_dir="/home/naagnes/taskonomy_full_dataset/"
+train_models="train_models.txt"
+val_models="val_models.txt"
 model_name="xception_taskonomy_new"
 #model_name="resnet18_taskonomy"
 #model_name="bugnet_taskonomy"
-tasks_to_train_on="dnkts"
+tasks_to_train_on="dnks"
 model_dir="test_run/"
 number_of_workers="1"
 experiment_name="dry_run"
-batch_size="10" # 5 is good for xception with 5 tasks.
+batch_size="5" # 5 is good for xception with 5 tasks.
 epochs="20"
 partition=0
 
 # Compile command string
 command="python train_taskonomy.py "
 command+="--data_dir $data_dir "
+command+="-tm $train_models "
+command+="-vm $val_models "
 command+="--arch $model_name "
 command+="--tasks $tasks_to_train_on "
 command+="--model_dir $model_dir "
